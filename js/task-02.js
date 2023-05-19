f78d4a75a65d2b66471b2d5b6a5f958d06090dfc
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-05-19 03:51:36 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+const elements = ingredients.map((el) => {
+  const listRef = document.createElement("li");
+  listRef.classList.add("item");
+  listRef.textContent = el;
+  console.log(listRef);
+  return listRef;
+});
+
+ingredientsRef.append(...elements);
