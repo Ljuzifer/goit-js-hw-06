268cff408c9f09bf6514de64b4c935985c24fb47
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-05-22 21:56:48 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -5,21 +5,18 @@ loginForm.addEventListener("submit", onGetFormData);
 function onGetFormData(event) {
   event.preventDefault();
 
-  const inputs = document.querySelectorAll("input");
+  const { email, password } = event.currentTarget.elements;
+  // console.log(event.currentTarget.elements);
 
-  if (inputs[0].value.trim() === "" || inputs[1].value.trim() === "") {
+  if (email.value.trim() === "" || password.value.trim() === "") {
     return alert("All fields must be filled!");
   }
 
-  const formElements = event.currentTarget.elements;
-
-  const email = formElements.email.value;
-  const password = formElements.password.value;
-
   const formData = {
-    email,
-    password,
+    email: email.value,
+    password: password.value,
   };
+
   loginForm.reset();
   console.log(formData);
 }
